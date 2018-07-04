e5a51903eb9f5bf75f316f6761a870e794542b61
Oscar <oscarwasil@gmail.com>
2018-07-03 21:06:24 -0500

    Added session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  # STEP 1: A ROUTE triggers a controller action
-  # verb "/urls" => "namespace/controllers#action"
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
 end
